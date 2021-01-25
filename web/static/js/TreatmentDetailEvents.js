763c867dd05bf09f23178e306ac9719dfa4a5366
unknown <nick-mozg1@mail.ru>
2021-01-25 12:20:31 +0500

    similar happening front fixed

--- web/static/js/TreatmentDetailEvents.js
+++ web/static/js/TreatmentDetailEvents.js
@@ -1,4 +1,5 @@
 let setActive = false;
+let leavingTime = 200;
 $('#sidebar').toggleClass('active');
 
 $(document).ready(function () {
@@ -6,30 +7,48 @@ $(document).ready(function () {
         window.history.back();
     })
 
-    $('#sidebarCollapse').on('click', function () {
+    $('#sidebarCollapse').on('click', function () {``
+        $('#sidebar').toggleClass('d-none');
+
         document.getElementById("open-buttons").style.zIndex = "-1"
         document.getElementById("data").style.display = "block";
         document.getElementById("simular").style.display = "none";
-        $('#sidebar').toggleClass('active');
+
+        setTimeout(function(){
+          $('#sidebar').toggleClass('active');
+          }, 10);
     });
 
     $('#sidebarCollapse2').on('click', function () {
-        document.getElementById("open-buttons").style.zIndex = "1";
         $('#sidebar').toggleClass('active');
+        document.getElementById("open-buttons").style.zIndex = "1";
+        setTimeout(function(){
+          $('#sidebar').toggleClass('d-none');
+          }, leavingTime);
     });
 
     $('#simularCollapse').on('click', function () {
+        $('#sidebar').toggleClass('d-none');
+
         document.getElementById("open-buttons").style.zIndex = "-1";
         document.getElementById("data").style.display = "none";
         document.getElementById("simular").style.display = "block";
-        $('#sidebar').toggleClass('active');
+
+        setTimeout(function(){
+          $('#sidebar').toggleClass('active');
+          }, 10);
     });
 
     $('#simularCollapse2').on('click', function () {
+        $('#sidebar').toggleClass('active');
+
         document.getElementById("open-buttons").style.zIndex = "1";
         document.getElementById("data").style.display = "none";
         document.getElementById("simular").style.display = "block";
-        $('#sidebar').toggleClass('active');
+
+         setTimeout(function(){
+          $('#sidebar').toggleClass('d-none');
+          }, leavingTime);
     });
 });
 
@@ -37,8 +56,10 @@ let isOpen = false;
 
 function imgFullScreenOpen(target) {
     $("#imgFullScreen > img").attr("src", target.children[0].src);
+
     document.getElementById("imgFullScreen").style.display = "flex"
     document.getElementById("imgFullScreen").style.opacity = "1";
+
     isOpen = !isOpen;
 }
 
